c576a220c33132813bfe8613a73b04a71970e2a2
sakodrake <hunterdrake2229@gmail.com>
2025-08-09 04:56:36 -0400

    is this thing working?

---
+++ day_1/helloworld.py
@@ -0,0 +1,55 @@
+import sys
+
+#Exercise: Level 1
+
+#1
+print(sys.version)
+
+#2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+#3
+print('sakodrake')
+print('toto')
+print('United States of America')
+print('I am enjoying 30 days of python')
+
+#4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('sakodrake'))
+print(type('toto'))
+print(type('United States of America'))
+
+#Exercise: Level 2
+"""I made the folder day_1 
+and put helloworld.py in it"""
+
+#Exercise: Level 3
+
+#1
+print("This is an Integer:", 10)
+print("This is a Float:", 9.8)
+print("This is a Complex Number:", 4-4j)
+print("This is a String:", 'sakodrake')
+print("This is a Boolean:", True)
+print("This is a List:", ['Asabeneh', 'Python', 'Finland'])
+print("This is a Tuple:", ('Asabeneh', 'Python', 'Finland'))
+print("This is a Set:", {'Asabeneh', 'Python', 'Finland'})
+print("This is a Dictionary:", {'name': 'Asabeneh', 'country': 'Finland', 'city': 'Helsinki'})
+
+#2
+#use pythag theorem (a^2 + b^2 = c^2)
+a = 8**2
+b = 5**2
+c = (a + b)**0.5
+print("The Euclidian distance between (2,3) and (10,8) is:", c)
